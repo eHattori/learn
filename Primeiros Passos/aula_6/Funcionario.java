792b09b7b9116f804ea5bbdf1945e596a31fad6d
Eduardo Hattori <eduardo.hattori@hotmail.com>
2017-03-17 10:24:22 -0300

    Aula 7 métodos estaticos

--- Primeiros Passos/aula_6/Funcionario.java
+++ Primeiros Passos/aula_6/Funcionario.java
@@ -1,9 +1,18 @@
 class Funcionario {
 
     private String nome;
+    private static int id = 1;
+
+    public getId(){
+        return this.id;
+    }
     
     public Funcionario(String nome){
         this.nome = nome;
+        this.id++;
     }
 
+    public Funcionario(){
+        this.id++;
+    }
 }
